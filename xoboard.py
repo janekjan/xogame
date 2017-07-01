0108307bdfe10cf6d8cb7be30154676ca8e1a08a
janekjan <jas.piskorski@spoko.pl>
2017-07-01 22:39:54 +0200

    NextMoveProvider should work

--- xoboard.py
+++ xoboard.py
@@ -8,6 +8,14 @@ class xoBoard:
     def GetBoard(self):
         return self.board
 
+    def GetEmptyPos(self):
+        retValue = []
+        for i in range(3):
+            for j in range(3):
+                if self.board[i][j]==0:
+                    retValue.append([i,j])
+        return retValue
+
     def WinningPos(self, player):
         if (player != 1) or (player != 2):
             raise RuntimeError("Player must be 1 or 2")
@@ -88,7 +96,79 @@ class xoBoard:
         self.board[x][y] = state
 
 class NextMoveProvider:
-    def __init__(self):
-        pass
+    def __init__(self, depth, player):
+        if depth > 4:
+            raise RuntimeError("Depth exceeding possible moves (4)")
+        if depth <= 0:
+            raise RuntimeError("Depth must be positive")
+        self.depth = depth
+        self.player = player
+        if player==1: self.opponent = 2
+        elif player==2: self.opponent = 1
+        else:
+            raise RuntimeError("Player must be 1 or 2")
+
+    def CalculateWeight(self, board):
+        if board.HasWon(self.opponent):
+            return [1, board]
+        if board.HasWon(self.player):
+            return [5, board]
+        if board.WinningPos(self.opponent) != []:
+            return [2, board]
+        if board.WinningPos(self.player) != []:
+            return [4, board]
+        return [3, board]
+
+    def NextMove(self, board, depth):
+        if depth==0:
+            return self.CalculateWeight(board)
+        #generate possibe self.player's moves
+        #need to use deepcopy techinique so as to obtain a separate object
+        boardTable = board.GetBoard()
+        myMoves = []
+        for i in range(3):
+            for j in range(3):
+                if boardTable[i][j]==0:
+                    myMoves.append(xoBoard(boardTable))
+                    myMoves[-1].FlipState(i, j, self.player)
+                    #instant win
+                    if myMoves[-1].HasWon(self.player):
+                        return [5, myMoves[-1]]
+        #for every possible move get opponent's assumed best move
+        opponentsBestMoves = []
+        for i in myMoves:
+            opponentsBestMoves.append(self.OpponentsMove(i, depth))
+        #calculate my best move
+        highestWeight = 0
+        for i in opponentsBestMoves:
+            if i[0]>highestWeight:
+                highestWeight = i[0]
+                chosenMove = i
+        return chosenMove
+    
+    def OpponentsMove(self, board, depth):
+        boardTable = board.GetBoard()
+        opponentMoves = []
+        for i in range(3):
+            for j in range(3):
+                if boardTable[i][j]==0:
+                    opponentMoves.append(xoBoard(boardTable))
+                    opponentMoves[-1].FlipState(i, j, self.opponent)
+                    #instant loose
+                    if opponentMoves[-1].HasWon(self.opponent):
+                        return [1, opponentMoves[-1]]
+        # pass to my next move
+        myNextMoves = []
+        for i in opponentMoves:
+            myNextMoves.append(self.NextMove(i, depth-1))
+        #assume best possible opponent's move
+        lowestWeight = 6
+        for i in myNextMoves:
+            if i[0]<lowestWeight:
+                lowestWeight = i[0]
+                chosenMove = i
+        return chosenMove
+    
+                    
 
     
